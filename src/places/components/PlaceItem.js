3284591cf214049ea61674dd949a7f4812c28e71
walecznyopos <walecznyopos@gmail.com>
2020-08-12 22:14:28 +0200

    first commit

---
+++ src/places/components/PlaceItem.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+
+import Card from '../../shared/components/UIElements/Card';
+import Button from '../../shared/components/FormElements/Button';
+import Modal from '../../shared/components/UIElements/Modal';
+import Map from '../../shared/components/UIElements/Map';
+import './PlaceItem.css';
+
+const PlaceItem = props => {
+  const [showMap, setShowMap] = useState(false);
+  const [showConfirmedModal, setShowComfirmedModal] = useState(false);
+
+  const openMapHandler = () => setShowMap(true);
+  const closeMapHandler = () => setShowMap(false);
+
+  const showDeleteWarningHandler = () => {
+    setShowComfirmedModal(true);
+  };
+
+  const cancelDeleteHandler = () => {
+    setShowComfirmedModal(false);
+  };
+
+  const confirmDeleteHandler = () => {
+    console.log(' deleting ... ');
+    setShowComfirmedModal(false);
+  };
+
+  return (
+    <React.Fragment>
+      <Modal
+        show={showMap}
+        onCancel={closeMapHandler}
+        header={props.address}
+        contentClass="place-item__modal-content"
+        footerClass="place-item__modal-actions"
+        footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
+      >
+        <div className="map-container">
+          <Map center={props.coordinates} zoom={16} />
+        </div>
+      </Modal>
+
+      <Modal 
+      show={ showConfirmedModal }
+      onCancel={ cancelDeleteHandler }
+      header="Are you sure?" 
+      footerClass="place-item__model-actions" 
+      footer={
+        <React.Fragment>
+          <Button onClick={ cancelDeleteHandler }  inverse> Cancel </Button>
+          <Button onClick={ confirmDeleteHandler }  inverse> Delete  </Button>
+        </React.Fragment>
+      }>
+        <p>
+          Do you want to procced and delete this place ?
+          Please note that it can't be undone thereafer.
+        </p>
+        
+      </Modal>
+      <li className="place-item">
+        <Card className="place-item__content">
+          <div className="place-item__image">
+            <img src={props.image} alt={props.title} />
+          </div>
+          <div className="place-item__info">
+            <h2>{props.title}</h2>
+            <h3>{props.address}</h3>
+            <p>{props.description}</p>
+          </div>
+          <div className="place-item__actions">
+            <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
+            <Button to={`/places/${props.id}`}>EDIT</Button>
+            <Button onClick={ showDeleteWarningHandler } danger>DELETE</Button>
+          </div>
+        </Card>
+      </li>
+    </React.Fragment>
+  );
+};
+
+export default PlaceItem;
